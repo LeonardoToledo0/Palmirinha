5748b57a038dcefc5ee1b2c90b7ecf80ca28bde9
Leonardo Toledo <leotoledo010@gmail.com>
2023-11-08 20:56:36 -0300

    Versão inicial do projeto

---
+++ ReceitaApp/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from ReceitaApp.models import Receita,Categoria
+
+# Register your models here.
+
+
+admin.site.register(Receita)
+admin.site.register(Categoria)
---
+++ ReceitaApp/models.py
@@ -0,0 +1,34 @@
+from django.db import models
+
+# Create your models here.
+
+
+
+
+dificuldade = [
+    ('F','Facil'),
+     ('M','Moderado'),
+     ('D','Dificil')]
+
+
+
+
+
+class Categoria(models.Model):
+    nome = models.CharField(max_length=30)
+
+
+    def __str__(self):
+        return self.nome
+
+
+class Receita(models.Model):
+    nome = models.CharField(max_length=50)
+    ingredientes = models.TextField(max_length=2000)
+    modo_de_preparo = models.TextField(max_length=8000)
+    grau_de_dificuldade = models.CharField(max_length=10,choices=dificuldade)
+    categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.nome
+
