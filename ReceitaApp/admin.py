dc0253d192ddd86656471b48cb968163d934c5f6
Leonardo Toledo <leotoledo010@gmail.com>
2023-11-08 22:17:51 -0300

    Adicionando link para image

--- ReceitaApp/admin.py
+++ ReceitaApp/admin.py
@@ -3,7 +3,15 @@ from django.contrib import admin
 from ReceitaApp.models import Receita,Categoria
 
 # Register your models here.
+class ReceitaAdmin(admin.ModelAdmin):
+    list_display = ['nome','grau_de_dificuldade', 'categoria']
+    list_filter = ['nome', 'grau_de_dificuldade', 'categoria']
+    list_display_links = ['nome','grau_de_dificuldade','categoria']
 
 
-admin.site.register(Receita)
+
+
+
+
+admin.site.register(Receita,ReceitaAdmin)
 admin.site.register(Categoria)
--- ReceitaApp/models.py
+++ ReceitaApp/models.py
@@ -16,7 +16,7 @@ dificuldade = [
 
 class Categoria(models.Model):
     nome = models.CharField(max_length=30)
-
+   
 
     def __str__(self):
         return self.nome
@@ -28,6 +28,11 @@ class Receita(models.Model):
     modo_de_preparo = models.TextField(max_length=8000)
     grau_de_dificuldade = models.CharField(max_length=10,choices=dificuldade)
     categoria = models.ForeignKey(Categoria,on_delete=models.CASCADE)
+    imagem = models.ImageField(null=True,upload_to='imagens/')
+
+
+
+
 
     def __str__(self):
         return self.nome
